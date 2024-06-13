f9528786465cfe9cf4332f572234dd88092fcc57
geovane oliveira <go399567@gmail.com>
2024-06-13 13:14:33 -0300

    Adicionando conteúdo dinamicamente

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = " "
+
+}
